75c747c24077fb8631ce84af3473d47e189c0aa9
Johannes Pfeifer <jpfeifer@gmx.de>
2015-08-22 12:12:46 +0200

    Add file calling all mod-files for testing

---
+++ run_all_files.m
@@ -0,0 +1,28 @@
+addpath([pwd filesep 'FV_et_al_2007']); %add ABCD_test.mod
+cd('Ascari_Sbordone_2014')
+dynare Ascari_Sbordone_2014
+cd('../Aguiar_Gopinath_2007')
+dynare Aguiar_Gopinath_2007
+cd('../Caldara_et_al_2012')
+dynare Caldara_et_al_2012
+cd('../FV_et_al_2007')
+dynare FV_et_al_2007_ABCD
+cd('../Gali_2008')
+dynare Gali_2008_Chapter_2
+dynare Gali_2008_Chapter_3
+cd('../Gali_Monacelli_2005')
+dynare Gali_Monacelli_2005
+cd('../RBC_capitalstock_shock')
+dynare RBC_capitalstock_shock.mod
+cd('../RBC_news_shock_model')
+dynare RBC_news_shock_model
+cd('../SGU_2003')
+dynare SGU_2003.mod
+cd('../SGU_2004')
+dynare SGU_2004
+cd('../Sims_2012')
+dynare Sims_2012_RBC
+cd('../Smets_Wouters_2007')
+dynare Smets_Wouters_2007
+
+
